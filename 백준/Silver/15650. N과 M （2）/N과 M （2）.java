c4e1ea069d9512784d050fb102940b7f3bc3166a
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2024-01-31 11:38:09 +0900

    [Silver III] Title: N과 M (2), Time: 160 ms, Memory: 15908 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static int n;
+	static int m;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String[] input=new String[2];
+		input=br.readLine().split(" ");
+		n=Integer.parseInt(input[0]);
+		m=Integer.parseInt(input[1]);
+		
+		arr=new int[m];
+		
+		comp(0,0);
+	}
+
+	public static void comp(int start, int count) throws IOException {
+		if(count==m) {
+			for(int i:arr) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+			return;
+		}else {
+			for(int i=start;i<n;i++) {
+				//System.out.println("i"+(i+1)+" n"+n);
+				arr[count]=i+1;
+				comp(i+1,count+1);
+				
+			}
+		}
+	}
+}
