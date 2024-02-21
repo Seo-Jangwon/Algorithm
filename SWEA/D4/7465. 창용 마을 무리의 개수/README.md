9574955d60f49f4a6c3b789df4d844b75ad613bb
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2024-02-21 21:59:36 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 144 ms, Memory: 26,520 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/README.md
@@ -0,0 +1,15 @@
+# [D4] 창용 마을 무리의 개수 - 7465 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWngfZVa9XwDFAQU) 
+
+### 성능 요약
+
+메모리: 26,520 KB, 시간: 144 ms, 코드길이: 1,721 Bytes
+
+### 제출 일자
+
+2024-02-21 21:59
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Solution {
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M, cnt;
+	static int[] parent;
+	static int[] check;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		int T=Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++)
+		{
+			String[] input1=br.readLine().split(" ");
+			N=Integer.parseInt(input1[0]);//사람은 N번까지
+			M=Integer.parseInt(input1[1]);//M개의 줄에 거쳐 입력
+			cnt=0;
+			parent=new int[N+1];
+			check=new int[N+1];
+			
+			for(int i=1;i<N+1;i++) {
+				parent[i]=i;
+			}
+			
+			for(int i=0;i<M;i++) {
+				String[] input2=br.readLine().split(" ");
+				int num1=Integer.parseInt(input2[0]);
+				int num2=Integer.parseInt(input2[1]);
+				
+				union(num1, num2);
+				
+			}//end for i
+			
+			for(int i=1;i<N+1;i++) {
+				check[findSet(parent[i])]+=1;
+			}
+			for(int i=1;i<N+1;i++) {
+				if(check[i]>=1){
+					cnt+=1;
+				}
+			}
+			
+			bw.write("#"+tc+" "+Integer.toString(cnt));
+            bw.newLine();
+			bw.flush();
+			
+		}//end for tc
+	}//end main()
+	
+	static int findSet(int x) {
+		if(x==parent[x])return x;
+		else return findSet(parent[x]);
+	}//end findSet
+	
+	static void union(int x,int y) {
+		int fX=findSet(x);
+		int fY=findSet(y);
+		
+		if(fX!=fY) {
+			parent[fY]=fX;
+		}
+	}
+	
+	static boolean check(int x, int y) {
+		int fX=findSet(x);
+		int fY=findSet(y);
+		if(fX==fY) {
+			return true;
+		}
+		else return false;
+	}
+}
