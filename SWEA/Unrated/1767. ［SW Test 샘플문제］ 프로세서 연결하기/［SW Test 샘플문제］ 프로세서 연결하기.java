011a85ef843baec5afbbbfdfef5bb7037c43899f
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2024-03-03 15:29:22 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 203 ms, Memory: 24,856 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -5,114 +5,114 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
-class Solution {
+public class Solution {
 	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-	static int N, maxCore, minLen;
-	static int[][]map;
-	static int[]di= {1,-1,0,0};
-	static int[]dj= {0,0,-1,1};
-	static ArrayList<Core> list;
+	
+	static int[] dy= {1,-1, 0, 0};
+	static int[] dx= {0, 0,-1, 1};
+	static int[][] map;
+	
+	static int N;
+	static int numOfCore;
+	static int static_LnkedCore;
+	static int lenOfWire;
+	
+	static ArrayList<Core> coreList;
 	
 	static class Core{
-		int i;
-		int j;
-		boolean wall;
-		public Core(int i, int j, boolean wall) {
+		int y;
+		int x;
+		public Core(int y, int x) {
 			super();
-			this.i = i;
-			this.j = j;
-			this.wall = wall;
+			this.y = y;
+			this.x = x;
 		}
-	}//end Core
-
+	}
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		
 		int T=Integer.parseInt(br.readLine());
-		for(int tc = 1; tc <= T; tc++)
-		{	
+		T+=1;
+		for(int tc=1;tc<T;tc++) {
 			N=Integer.parseInt(br.readLine());
-			maxCore=0;
-			minLen=Integer.MAX_VALUE;//정답 저장할 최소값 초기화
 			map=new int[N][N];
-			list=new ArrayList<Core>();
+			coreList=new ArrayList<Core>();
+			
+			static_LnkedCore=0;
+			lenOfWire=Integer.MAX_VALUE;
+			numOfCore=0;
 			
 			for(int i=0;i<N;i++) {
 				String[] in=br.readLine().split(" ");
 				for(int j=0;j<N;j++) {
 					map[i][j]=Integer.parseInt(in[j]);
-					
-					boolean checkWall=false;
-					if(map[i][j]==1) {
-						if(i==0||j==0||i==N-1||j==N-1) checkWall=true;
-						list.add(new Core(i, j, checkWall));
-					}
-					
-				}//end for j
-			}//end for i
+					if(map[i][j]==1&&i!=0&&i!=N-1&&j!=0&&j!=N-1) {coreList.add(new Core(i,j)); numOfCore++;}
+				}
+			}//end for
 			
 			dfs(0,0,0);
 			
-			bw.write("#"+Integer.toString(tc)+" "+Integer.toString(minLen));
-			bw.newLine();			
+			bw.write("#"+Integer.toString(tc)+" "+Integer.toString(lenOfWire));
+			bw.newLine();
 			bw.flush();
-		}//end tc
+			
+		}//end test case;
 	}//end main
 	
-	
-	static void dfs(int dep, int core, int len) {
-
-		
-		if(dep==list.size()) {
-			if(maxCore<core) {
-				maxCore=core;
-				minLen=len;
-			}else if(maxCore==core) {
-				if(minLen>len) {
-					minLen=len;
+	static void dfs(int dep, int linkedCore, int len) {
+		if(dep==numOfCore) {
+			if(static_LnkedCore<linkedCore) {
+				static_LnkedCore=linkedCore;
+				lenOfWire=len;
+			}
+			if(static_LnkedCore==linkedCore) {
+				if(lenOfWire>len) {
+					lenOfWire=len;
 				}
 			}
 			return;
 		}//end 종료조건
-
-		if(!list.get(dep).wall) {
-			int idxI=list.get(dep).i;
-			int idxJ=list.get(dep).j;
-			
-			for(int d=0;d<4;d++) {
-				if(check(idxI,idxJ,d)) {
-					int wire=setWire(idxI, idxJ, d, 5);
-					dfs(dep+1,core+1,len+wire);
-					setWire(idxI, idxJ, d, 0);
-				}//end if
-			}//end for d
 		
-		}
-		dfs(dep+1,core,len);//연결 안하는 경우
+		Core curr=coreList.get(dep);
+		int currY=curr.y;
+		int currX=curr.x;
+		
+		for(int d=0;d<4;d++) {
+			if(check(currY, currX, d)) {
+				int wireLen=link(currY, currX, d, 5);
+				dfs(dep+1, linkedCore+1, len+wireLen);
+				link(currY, currX, d, 0);
+			}
+		}//end 4방향 탐색
+		dfs(dep+1, linkedCore, len);//연결 안시킴
 	}//end dfs
-	
-	static boolean check(int i, int j, int d) {//코어나 전선이 없는지 확인
-		int ni=i+di[d];
-		int nj=j+dj[d];
+
+	static boolean check(int y, int x, int d) {//y좌표, x좌표, 방향
+		int ny=y+dy[d];
+		int nx=x+dx[d];
+		boolean check=false;
 		while(true) {
-			if(ni<0||nj<0||ni>=N||nj>=N)return true;
-			if(map[ni][nj]>0) return false;
-			ni+=di[d];
-			nj+=dj[d];
+			if(ny==-1||nx==-1||ny==N||nx==N) {check=true; break;}
+			if(map[ny][nx]!=0) {check=false;break;}
+			ny+=dy[d];
+			nx+=dx[d];
 		}
-	}
-	static int setWire(int i, int j, int d, int n) {//코어에 전선 연결, 지우기
-		int ni=i+di[d];
-		int nj=j+dj[d];
-		int len=0;
-		while(ni>=0&&nj>=0&&ni<N&&nj<N) {
-			map[ni][nj]=n;
-			len++;
-			ni+=di[d];
-			nj+=dj[d];
-		}
-		return len;
-	}
+		return check;
+	}//end check
 	
-}//end class
+	static int link(int y, int x, int d, int val) {//y좌표, x좌표, 방향, 값(연결 : 5, 연결 끊기: 0)
+		int ny=y+dy[d];
+		int nx=x+dx[d];
+		int wireLen=0;
+		while(true) {
+			if(ny==-1||nx==-1||ny==N||nx==N) {break;}
+			map[ny][nx]=val;
+			wireLen++;
+			ny+=dy[d];
+			nx+=dx[d];
+		}
+		return wireLen;
+	}//end link
+	
+}
