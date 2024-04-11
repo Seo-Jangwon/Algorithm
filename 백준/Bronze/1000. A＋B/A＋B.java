78890f775bf94955e55ae36c1ba21880f2ab6dd5
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2024-04-11 11:25:30 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int a, b;
+		String[] in = br.readLine().split(" ");
+		a = Integer.parseInt(in[0]);
+		b = Integer.parseInt(in[1]);
+		System.out.println(a+b);
+	}
+
+}
