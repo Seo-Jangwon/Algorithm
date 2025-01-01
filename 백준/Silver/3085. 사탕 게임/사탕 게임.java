ea87deb078f35bb5244341c46112a21d9da929cc
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2025-01-01 15:18:26 +0900

    [Silver II] Title: 사탕 게임, Time: 308 ms, Memory: 14940 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -3,110 +3,106 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-	static int N,max;
-	static String[][] arr;
-	static int[] dy= {1,0,-1,0};
-	static int[] dx= {0,1,0,-1};
-	
-	static class Point{
-		int y;
-		int x;
-		public Point(int y, int x) {
-			super();
-			this.y = y;
-			this.x = x;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		N=Integer.parseInt(br.readLine());
-		
-		arr=new String[N][N];
-		
-		for(int i=0;i<N;i++) {
-			String[]in2=br.readLine().split("");
-			for(int j=0;j<N;j++) {
-				arr[i][j]=in2[j];
-			}
-		}//end for
-		
-		max=0;
-		change();
-		bw.write(Integer.toString(max));
-		bw.flush();
-	}//end main
 
-	static void change() {
-		boolean[][][]changed=new boolean[N][N][4];
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				for(int d=0;d<4;d++) {
-					int ny=dy[d]+i;
-					int nx=dx[d]+j;
-					if(ny>=0&&nx>=0&&ny<N&&nx<N&&!arr[ny][nx].equals(arr[i][j])&&!changed[i][j][d]) {
-						String tmp;
-						tmp=arr[ny][nx];
-						arr[ny][nx]=arr[i][j];
-						arr[i][j]=tmp;
-						changed[i][j][d]=true;
-						d=(d+2)%4;
-						changed[ny][nx][d]=true;
-						
-						find();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-						tmp=arr[ny][nx];
-						arr[ny][nx]=arr[i][j];
-						arr[i][j]=tmp;
-					}//end if
-				}//end for d
-			}
-		}//end for
-	}//end change
-	
-	
-	static void find() {
-		String now="";
-		int cnt;
-		for(int i=0;i<N;i++) {
-			cnt=0;
-			for(int j=0;j<N;j++) {
-				if(!arr[i][j].equals(now)) {
-					now=arr[i][j];
-					cnt=0;
-				}if(arr[i][j].equals(now)) {
-					cnt+=1;
-				}
-				if(cnt>max) {
-					max=cnt;
-				}
-			}
-		}
-		for(int j=0;j<N;j++) {
-			cnt=0;
-			for(int i=0;i<N;i++) {
-				if(!arr[i][j].equals(now)) {
-					now=arr[i][j];
-					cnt=0;
-				}if(arr[i][j].equals(now)) {
-					cnt+=1;
-				}
-				if(cnt>max) {
-					max=cnt;
-				}
-			}
-		}
-		
-		
-	}//end dfs
-	
+    static int n;
+    static int ans;
+    static String[][] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        n = Integer.parseInt(br.readLine());
+
+        ans = 0;
+        arr = new String[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split("");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = input[j];
+            }
+        }// end for
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // 1. 아래로 뒤집기
+                int newy = i + 1;
+                int newx = j;
+
+                if (newy < n) {
+                    String s = arr[i][j];
+                    String s1 = arr[newy][newx];
+                    arr[i][j] = s1;
+                    arr[newy][newx] = s;
+
+                    // 2. 연속되는 곳 검사
+                    check();
+
+                    // 3. 원상복구
+                    arr[newy][newx] = s1;
+                    arr[i][j] = s;
+                }
+
+                //----------
+
+                // 1. 우측으로 뒤집기
+                newy = i;
+                newx = j + 1;
+
+                if (newx < n) {
+                    String s = arr[i][j];
+                    String s1 = arr[newy][newx];
+                    arr[i][j] = s1;
+                    arr[newy][newx] = s;
+
+                    // 2. 연속되는 곳 검사
+                    check();
+
+                    // 3. 원상복구
+                    arr[newy][newx] = s1;
+                    arr[i][j] = s;
+                }
+
+            }
+        }// end for
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+
+    }// end main
+
+    static void check() {
+        for (int i = 0; i < n; i++) {
+            int count = 1; // 현재 연속된 개수
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j].equals(arr[i][j - 1])) {
+                    count++;
+                } else {
+                    ans = Math.max(ans, count);
+                    count = 1;
+                }
+            }
+            ans = Math.max(ans, count);
+        }
+
+        for (int j = 0; j < n; j++) {
+            int count = 1;
+            for (int i = 1; i < n; i++) {
+                if (arr[i][j].equals(arr[i - 1][j])) {
+                    count++;
+                } else {
+                    ans = Math.max(ans, count);
+                    count = 1;
+                }
+            }
+            ans = Math.max(ans, count);
+        }
+    }// end check function
+
 }
-	
