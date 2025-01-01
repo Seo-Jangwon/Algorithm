8d96c2f0cf9026170673f454ef6aa2440b01a1bf
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2025-01-02 00:57:40 +0900

    [Gold V] Title: 암호 만들기, Time: 148 ms, Memory: 15292 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -3,59 +3,79 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringBuilder sb=new StringBuilder();
-	static int L,C;
-	static String[] alphabet;
-	static boolean[] v;
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		String[] input1=br.readLine().split(" ");
-		L=Integer.parseInt(input1[0]);
-		C=Integer.parseInt(input1[1]);
-		
-		v=new boolean[L];
-		
-		alphabet=br.readLine().split(" ");
-		Arrays.sort(alphabet);
 
-		
-		for(int i=0;i<C;i++) {
-			dfs(1,i+1,alphabet[i]);
-		}
-	}
-	static void dfs(int dep, int start,String s) throws IOException {
-		if(dep==L) {
-			if(check(s)) {
-			bw.write(s);
-			bw.newLine();
-			bw.flush();
-			}
-		}//end if
-		
-		for(int i=start;i<C;i++) {
-			dfs(dep+1,i+1,s+alphabet[i]);
-		}
-		
-	}
-	
-	static boolean check(String s) {
-		int ja=0;
-		int mo=0;
-		for(int i=0;i<s.length();i++) {
-			char c=s.charAt(i);
-			if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u') {
-				mo+=1;
-			}
-			else {ja+=1;}
-		}
-		if(ja>=2&&mo>=1) {
-			return true;
-		}else return false;
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int l, c;
+    static String[] str;
+    static List<String> pwd;
+
+    public static void main(String[] args) throws IOException {
+        String[] in1 = br.readLine().split(" ");
+        l = Integer.parseInt(in1[0]);
+        c = Integer.parseInt(in1[1]);
+
+        str = new String[c];
+        pwd = new ArrayList<>();
+        str = br.readLine().split(" ");
+        Arrays.sort(str);
+
+        dfs(0, 0);
+        bw.close();
+
+    }
+
+    static void dfs(int idx, int dep) throws IOException {
+        if (dep == l) {
+            if (validVowls() && validConsonants()) {
+                for (String str : pwd) {
+                    bw.write(str);
+                    bw.flush();
+                }
+                bw.newLine();
+                bw.flush();
+            }
+            return;
+        }// end for
+
+        for (int i = idx; i < c; i++) {
+            pwd.add(str[i]);
+            dfs(i + 1, dep + 1);
+            pwd.remove(str[i]);
+        }
+
+    }// end dfs func
+
+    static boolean validVowls() {// 하나 이상의 모음이 포함되었는지 확인
+        for (String str : pwd) {
+            if (str.equals("a") || str.equals("e") || str.equals("i") || str.equals("o")
+                || str.equals("u")) {
+                return true;
+            }
+        }//end for
+        return false;
+    }// end valid func
+
+    static boolean validConsonants() {// 두 개 이상의 자음이 들어가는지
+        int cnt = 0;
+        for (String str : pwd) {
+            if (str.equals("a") || str.equals("e") || str.equals("i") || str.equals("o")
+                || str.equals("u")) {
+
+            } else {
+                cnt++;
+                if (cnt == 2) {
+                    return true;
+                }
+            }
+        }//end for
+        return false;
+    }// end valid func
 
 }
