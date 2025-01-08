dc22113b42e1be83a599b88490bb41e15a79fc61
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2025-01-08 10:31:05 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 632 ms, Memory: 151516 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int[] arr;
+    static List<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new int[n];
+        String[] in = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(in[i]);
+        }
+
+        list = new ArrayList<>();
+        list.add(arr[0]);
+        for (int i = 0; i < n; i++) {
+            put(arr[i]);
+        }
+
+        bw.write(String.valueOf(list.size()));
+        bw.flush();
+        bw.close();
+
+    }
+
+    static void put(int val) {
+        int start = 0;
+        int end = list.size() - 1;
+
+        if (list.get(end) < val) {
+            list.add(val);
+        } else {
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (list.get(mid) < val) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        list.set(start, val);
+        }
+    }
+}
