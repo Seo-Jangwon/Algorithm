1e92178a882a78e25cbe6f903cf070aed13d89f0
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2025-01-08 10:29:17 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 572 ms, Memory: 139260 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,11 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -13,46 +11,47 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int n;
     static int[] arr;
     static List<Integer> list;
 
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        list = new ArrayList<Integer>();
+        int n = Integer.parseInt(br.readLine());
 
+        arr = new int[n];
         String[] in = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(in[i]);
         }
 
+        list = new ArrayList<>();
         list.add(arr[0]);
         for (int i = 0; i < n; i++) {
-            putNum(arr[i]);
+            put(arr[i]);
         }
 
         bw.write(String.valueOf(list.size()));
         bw.flush();
         bw.close();
 
-    }//end main
+    }
 
-    static void putNum(int num) {
-        int start = 0, end = list.size() - 1; //다 index
-        if(list.get(end)< num) {list.add(num);}
-        else{
+    static void put(int val) {
+        int start = 0;
+        int end = list.size() - 1;
 
+        if (list.get(end) < val) {
+            list.add(val);
+        } else {
             while (start <= end) {
                 int mid = (start + end) / 2;
-                if (list.get(mid) < num) {// 리스트의 중간값이 내가 넣어야 하는 숫자보다 작다면
+                if (list.get(mid) < val) {
                     start = mid + 1;
-                } else {//리스트의 중간값이 내가 넣어야 하는 숫자보다 크거나 같다면
+                } else {
                     end = mid - 1;
                 }
             }
-            list.set(start, num);
+list.set(start, val);
         }
+       
     }
-
 }
