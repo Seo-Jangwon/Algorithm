de7a401d7c2a68c941e8d80460277aa7607b73f3
서장원 <90087083+Seo-Jangwon@users.noreply.github.com>
2025-02-14 20:35:42 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,5 @@
+-- 코드를 작성해주세요
+select distinct d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+from SKILLCODES as s join DEVELOPERS as d on d.SKILL_CODE & s.CODE=s.CODE
+where s.CATEGORY='Front End'
+order by d.ID 
